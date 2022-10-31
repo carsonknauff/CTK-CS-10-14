8bc330153bbd515dab47f93db6791a5e5e05c791
carsonknauff <115803891+carsonknauff@users.noreply.github.com>
2022-10-31 15:08:25 +0000

    Stuff

--- script.js
+++ script.js
@@ -5,12 +5,28 @@
 
 function submitForm(name, hometown) {
   // search the DOM for a specific tag with id "guestlist"
-  var guestlist = document.getElementById("guetstlist")
-  var n =  document.getElementById("name")
-  var h =  document.getElementById("hometown")
-  guestlist.innerHTML = "<lil>" + name + "." + hometown
+  var guestlist = document.getElementById("guestlist");
+  var n =  document.getElementById("name");
+  var h =  document.getElementById("hometown");
+  guestlist.innerHTML += "<li>" + name.value + ". " + hometown.value + "</li>";
   n.value = "";
   h.value = "";
   n.focus();
+  return false;
+}
+
+function checkStorage(){
+var answerlist = document.getElementById("answerlist");
+var currentanswers = localStorage.getItem("answerlist", answer.list);
+answerlist.innterHTML = currentanswers;
+}
+
+function submitAnswer(answer) {
+  // search the DOM for a specific tag with id "guestlist"
+  var answerlist = document.getElementById("answerlist");
+  localStorage.setItem("answerlist", answer.value);
+  answerlist.innerHTML += answer.value + "<br />";
+  answer.value = "";
+  answer.focus();
   return false;
 }
